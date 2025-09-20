cd26c0b3db0284c47814d08fbd6d3cf2734cb278
Esmail <ae1652003@gmail.com>
2025-09-20 14:15:40 +0300

    esmail updated

--- miniproejct.py
+++ miniproejct.py
@@ -25,3 +25,22 @@ while True:
     if time.time() - last_time > 1:
         send_data()
         last_time=time.time()
+#################################################
+
+import board
+import busio
+from adafruit_pn532.i2c import PN532_I2C
+
+# I2C connection setup
+i2c = busio.I2C(board.SCL, board.SDA)
+pn532 = PN532_I2C(i2c, debug=False)
+
+# Configure PN532 to read NFC tags
+pn532.SAM_configuration()
+
+print("Waiting for an NFC tag...")
+
+while True:
+    uid = pn532.read_passive_target(timeout=0.5)
+    if uid is not None:
+        print(f"Found NFC tag with UID: {uid.hex()}")
